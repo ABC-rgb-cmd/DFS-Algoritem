e9cf3897a3e08b3748f045c1943b949255c6bba8
EnchantedMax <52086135+EnchantedMax@users.noreply.github.com>
2025-03-21 14:22:32 +0100

    Mainapp Base

--- DFS/DFS.cpp
+++ DFS/DFS.cpp
@@ -1,20 +1,34 @@
-// DFS.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+#include <fstream>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    bool enabled = true;
+    int choice;
+    while (enabled)
+    {
+        cout << "Iskanje v Globino - Izbira \n1 Preberi graf iz datoteke\n2 Pozeni iskanje iz vozilisca s\n"
+            <<"3 Izpis seznama vozilisc in njihovih podaktov\n4 Izpis poti med voziliscema s in d\n5 Konec";
+        switch (choice)
+        {
+        case 1:
+            //Read Graph
+            break;
+        case 2:
+            //Search
+            break;
+        case 3:
+            //info dump
+            break;
+        case 4:
+            //path output
+            break;
+        case 5:
+            enabled = false;
+            break;
+        default:
+            break;
+        }
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
