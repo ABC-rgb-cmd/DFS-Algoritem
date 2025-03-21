06963dee1ae72d8469fb94f2587d0114cecab937
EnchantedMax <52086135+EnchantedMax@users.noreply.github.com>
2025-03-21 18:18:37 +0100

    DFS Searching

--- DFS/DFS.cpp
+++ DFS/DFS.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <fstream>
+#include <stack>
+#include <vector>
+#include <string>
 using namespace std;
+struct Vozlisce {
+    int predhodnik;
+    int dolzina;
+    int status; // 0 = nepregledano, 1 = v obdelavi, 2 = razvito
+    int indeks;
+    string ime;
+};
 void readGraph(int& stevilo_vozlisc, int**& C)
 {
     ifstream f("graf.txt");
@@ -30,15 +40,61 @@ void readGraph(int& stevilo_vozlisc, int**& C)
         C[v2][v1] = cena;
     }
 }
+void depthSearch(int** C, int s, int stevilo_vozlisc) {
+    if (s < 0 || s >= stevilo_vozlisc) {
+        cout << "Invalid starting vertex!" << endl;
+        return;
+    }
+
+    vector<Vozlisce> vozlisca(stevilo_vozlisc);
+
+    // Inicializacija vseh vozlišè
+    for (int i = 0; i < stevilo_vozlisc; i++) {
+        vozlisca[i].status = 0;  // nepregledano
+        vozlisca[i].dolzina = -1;
+        vozlisca[i].predhodnik = -1;
+        vozlisca[i].indeks = i;
+        vozlisca[i].ime = "V" + to_string(i);
+    }
+
+    // Inicializacija zaèetnega vozlišèa
+    vozlisca[s].status = 1; // v obdelavi
+    vozlisca[s].dolzina = 0;
+    vozlisca[s].predhodnik = -1;
+
+    stack<int> sklad;
+    sklad.push(s);
+
+    while (!sklad.empty()) {
+        int v = sklad.top();
+        sklad.pop();
+
+        // Oznaèimo vozlišèe kot razvito
+        vozlisca[v].status = 2;
+
+        // Pregledamo vse sosede vozlišèa v
+        for (int u = 0; u < stevilo_vozlisc; u++) {
+            if (C[v][u] != 0 && vozlisca[u].status == 0) { // Èe obstaja povezava in ni obiskan
+                vozlisca[u].status = 1; // v obdelavi
+                vozlisca[u].dolzina = vozlisca[v].dolzina + 1;
+                vozlisca[u].predhodnik = v;
+                sklad.push(u);
+            }
+        }
+    }
+
+}
+
 int main()
 {
     int stevilo_vozlisc;
-    int** C;
+    int** C = nullptr;
     bool enabled = true;
     int choice;
+    int s;
     while (enabled)
     {
-        cout << "Iskanje v Globino - Izbira \n1 Preberi graf iz datoteke\n2 Pozeni iskanje iz vozlisca s\n"
+        cout << "\nIskanje v Globino - Izbira \n1 Preberi graf iz datoteke\n2 Pozeni iskanje iz vozlisca s\n"
             <<"3 Izpis seznama vozlisc in njihovih podaktov\n4 Izpis poti med vozliscema s in d\n5 Konec\n\n"
             <<"Vasa Izbira: ";
         cin >> choice;
@@ -50,6 +106,16 @@ int main()
             break;
         case 2:
             //Search
+            cout << "\nVpisite vozlisce s: ";
+            cin >> s;
+            if (C != nullptr)
+            {
+                depthSearch(C, s, stevilo_vozlisc);
+            }
+            else
+            {
+
+            }
             break;
         case 3:
             //info dump
